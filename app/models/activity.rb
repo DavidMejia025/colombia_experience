224af2323353791c51d4f57848220d10044b7d4c
david <davidndy@gmail.com>
2020-02-08 17:27:23 -0500

    add activities and quotes business logic

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,26 @@ class Activity < ApplicationRecord
     has_one    :tour
     has_one    :quote
     has_many   :variable_costs
+
+    def clone_variable_costs
+      self.variable_costs.map do |variable_cost|
+        variable_cost.dup
+      end
+    end
+
+    def clone_activity
+      self.dup
+    end
+
+    def calculate_cost
+      variable_cost_checked = self.variable_costs.where(checked: true).distinct.first
+
+      self.total = self.days * variable_cost_checked.cost
+    end
+
+    def calculate_cost_per_person
+      variable_cost_checked = self.variable_costs.where(checked: true).distinct.first
+
+      self.total_per_person = variable_cost_checked.cost * self.days / self.ocupation
+    end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,13 @@ class Quote < ApplicationRecord
     has_and_belongs_to_many :cities
     has_many :activities
     has_many :tours
+
+    def add_activity(activity: activity)
+      quote_activity                = activity.dup
+      quote_activity_variable_costs = activity.clone_variable_costs
+
+      self.activities << quote_activity
+
+      self.activities.last.variable_costs << quote_activity_variable_costs
+    end
 end
